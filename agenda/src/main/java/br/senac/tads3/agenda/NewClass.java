c106c33d0c831cfd13bc68a059a7e2ca7236e57c
alcides.elneto1 <alcides.elneto1@CAS555098W0731.senacsp.edu.br>
2016-08-19 20:24:45 -0300

    Teste

---
+++ agenda/src/main/java/br/senac/tads3/agenda/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads3.agenda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alcides.elneto1
+ */
+public class NewClass {
+    
+    public static void main(String[] args){
+        
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Hi! \nwhats your name: ");
+        ler.next();
+        
+    }
+}

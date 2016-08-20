f78d084e7ebce1428072411d4123f6de666c9735
alcides.elneto1 <alcides.elneto1@CAS555098W0731.senacsp.edu.br>
2016-08-19 21:08:42 -0300

    5 COMIIT

--- agenda/src/main/java/br/senac/tads3/agenda/NewClass.java
+++ agenda/src/main/java/br/senac/tads3/agenda/NewClass.java
@@ -13,10 +13,8 @@ import java.util.Scanner;
  */
 public class NewClass {
     
-    public static void main(String[] args){
-        
-   
-        System.out.println("I have a reservation: ");
+    public static void main(String[] args){           
+     System.out.println("I have a reservation: ");
               
     }
 }
